bfe33de1e23d170fc52f5c4f1ee8a601163489bd
Jayantjain1 <jjayant511@gmail.com>
2022-12-12 16:18:55 +0530

    Different message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,26 +3,29 @@ package com.driver;
 import java.util.*;
 import java.lang.*;
 
-public class Main{
-     static class Product{
-        public int product(int x,int y){
-            int a = x*y;
-            return a;
+public class Main
+{
+    public static class Product{
+
+        public int product(int a,int b){
+
+            return a*b ;
         }
         public int product(int a,int b,int c){
-            int ans = a*b*c;
-            return ans;
+
+            return a*b*c ;
         }
         public double product(double a,double b){
-            double ans = a*b;
-            return ans;
+
+            return (double)a*b ;
         }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Product obj = new Product();
-        obj.product(1,2);
-        obj.product(2,3,4);
-        obj.product(1.2,3.4);
+        Product p = new Product();
+
+        p.product(1,2);
+        p.product(2,3,4);
+        p.product(1.2,3.4);
     }
 }
